933e6c466d37a54781e62d83b01022b5bb48cd1b
Michael <michael@vm5.com>
2015-09-21 13:46:31 +0800

    now will automatically install base package

--- install.sh
+++ install.sh
@@ -22,11 +22,23 @@ change_file ${rc}
 done
 # check fish shell is exist
 fish_path=$(cat /etc/shells |grep fish)
-if [ $fish_path ]; then
-    chsh -s $fish_path
-else
-    echo '[WARNING]  Fish not installed'
+if [ ! $fish_path ]; then
+    test -e /etc/debian_version && DIST="Debian"
+    grep Ubuntu /etc/lsb-release &> /dev/null && DIST="Ubuntu"
+    if [ "$DIST" = "Ubuntu" ] || [ "$DIST" = "Debian" ]; then
+        install='sudo apt-get -y install'
+        remove='sudo apt-get -y remove'
+        pkginst='sudo dpkg -i'
+        $install git screen vim fish
+    fi
+    if ! echo $DIST | egrep 'Ubuntu|Debian'; then
+        echo '[WARNING]  Fish not installed'
+        echo '[WARNING]  Install.sh currently only supports Ubuntu and Debian.'
+        exit 1
+    fi
+    fish_path=$(cat /etc/shells |grep fish)
 fi
+chsh -s $fish_path
 cp -r .ssh/config ~/.ssh/config
 install -d ~/.config
 cp -r fish ~/.config
