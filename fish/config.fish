e1e065c418a71ba65acd4cb10542044331e7418e
Ta-Ching Chen <life1347@gmail.com>
2015-12-21 14:40:39 +0800

    add go path

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,6 @@ set fish_greeting ""
 set -xg LANG en_US.UTF-8
 set -xg LANGUAGE "en_US.UTF-8"
 set -xg LC_ALL en_US.UTF-8
+set -xg GOROOT /usr/local/go
+set -xg GOPATH $HOME/.go
+set -xg PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin $GOROOT/bin/
