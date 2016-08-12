7848a94c1de0dce2a7927dc02f5ef7110daf90fa
Ta-Ching <life1347@gmail.com>
2016-08-12 15:58:49 +0800

    update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,31 +1,59 @@
-function fish_prompt --description 'Write out the prompt'
-  set -l last_status $status
+# You can override some default options with config.fish:
+#
+#  set -g theme_short_path yes
 
-  # User
-  set_color green
-  echo -n (whoami)
+function fish_prompt
+  set -l last_command_status $status
+  set -l cwd
 
-  echo -n '@'
+  if test "$theme_short_path" = 'yes'
+    set cwd (basename (prompt_pwd))
+  else
+    set cwd (prompt_pwd)
+  end
 
-  # Host
-  echo -n (hostname -s)
-  set_color normal
+  set -l fish     "⋊>"
+  set -l ahead    "↑"
+  set -l behind   "↓"
+  set -l diverged "⥄ "
+  set -l dirty    "⨯"
+  set -l none     ""
 
-  set_color yellow
-  printf '(%s)' (date "+%H:%M:%S") 
-  set_color normal
+  set -l normal_color     (set_color normal)
+  set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
+  set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
+  set -l directory_color  (set_color $fish_color_quote ^/dev/null; or set_color brown)
+  set -l repository_color (set_color $fish_color_cwd ^/dev/null; or set_color green)
 
-  echo -n ':'
+  if test $last_command_status -eq 0
+    echo -n -s $success_color $fish $normal_color
+  else
+    echo -n -s $error_color $fish $normal_color
+  end
 
-  # PWD
-  set_color cyan
-  echo -n (pwd)
-  set_color normal
+  if git_is_repo
+    if test "$theme_short_path" = 'yes'
+      set root_folder (command git rev-parse --show-toplevel ^/dev/null)
+      set parent_root_folder (dirname $root_folder)
+      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
 
-  __terlar_git_prompt
+      echo -n -s " " $directory_color $cwd $normal_color
+    else
+      echo -n -s " " $directory_color $cwd $normal_color
+    end
+
+    echo -n -s " on " $repository_color (git_branch_name) $normal_color " "
+
+    if git_is_touched
+      echo -n -s $dirty
+    else
+      echo -n -s (git_ahead $ahead $behind $diverged $none)
+    end
+  else
+    echo -n -s " " $directory_color $cwd $normal_color
+  end
+
   echo
 
-  set_color red
-  echo -n '>> '
-  set_color normal
+  echo -n -s $error_color"» "
 end
