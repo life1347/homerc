a02c1282fe08e802b5a55326e3fdb4ed460f8a21
Ta-Ching <life1347@gmail.com>
2016-08-12 17:10:11 +0800

    update env

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,4 @@ set -xg LANGUAGE "en_US.UTF-8"
 set -xg LC_ALL en_US.UTF-8
 set -xg GOROOT /usr/local/go
 set -xg GOPATH $HOME/.go
-set -xg PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin $GOROOT/bin/
+set -xg PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin $GOROOT/bin/ $GOPATH/bin
